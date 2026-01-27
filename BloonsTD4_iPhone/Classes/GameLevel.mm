dadc416fef0752299df2d77a9cfe0885838b1555
polavux <polavux@proton.me>
2026-01-26 21:30:33 -0500

    Update GameLevel.mm
    testing porpoises

---
+++ BloonsTD4_iPhone/Classes/GameLevel.mm
@@ -0,0 +1,17 @@
+#include "GameLevel.h"
+
+void CGameLevelScreen::SetState(GAME_LEVEL_STATE state) {
+
+}
+
+virtual void CGameLevelScreen::CallBack(CBaseButton *param_1, void *param_2) {
+
+}
+
+virtual void CGameLevelScreen::Process(double param_1) {
+
+}
+
+void CGameLevelScreen::TestTouch() {
+
+}
